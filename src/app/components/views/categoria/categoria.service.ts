a26e9a9e442d9b81ba1f534f4cda578a58556b45
WagnerSolak <wwwagnerww@gmail.com>
2021-08-02 14:56:10 -0300

    Categoria DELETE implementado

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -19,11 +19,22 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(url);
   }
 
+  //Método para buscar uma Categoria para utilização no método delete
+  findById(id: String): Observable<Categoria>{
+    const url = `${this.baseUrl}/categorias/${id}`
+    return this.http.get<Categoria>(url)
+  }
+
   //Método para criar uma nova Categoria
   create(categoria: Categoria): Observable<Categoria>{
     //const url = '${this.baseUrl}/categorias'
     const url = this.baseUrl + "/categorias";
     return this.http.post<Categoria>(url, categoria);
+  }
+
+  delete(id: String): Observable<void>{
+    const url = `${this.baseUrl}/categorias/${id}`
+    return this.http.delete<void>(url)
   }
 
   mensagem(str: String): void {
